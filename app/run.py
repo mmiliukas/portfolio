9019abd6173a26a1a2f03f1d6f01356f0ec1698c
Mantas Miliukas <mmiliukas@gmail.com>
2026-04-07 22:47:27 +0300

    Initial commit

---
+++ app/run.py
@@ -0,0 +1,59 @@
+import logging
+from datetime import date, datetime
+from xml.etree import ElementTree
+
+import pandas as pd
+import requests
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+FILE = "./publications.csv"
+
+
+def as_date(value: str):
+    dt = datetime.strptime(value, "%a, %d %b %Y %H:%M:%S %z")
+    return dt.strftime("%Y-%m-%d")
+
+
+def download() -> pd.DataFrame:
+    logger.info("Downloading RSS feed from arXiv...")
+
+    url = "https://rss.arxiv.org/rss/q-fin.PM"
+
+    resp = requests.get(url, timeout=5)
+    resp.raise_for_status()
+
+    tree = ElementTree.fromstring(resp.content)
+
+    publications = []
+
+    for item in tree.findall(".//item"):
+        pubDate = as_date((item.findtext("pubDate") or "").strip())
+        title = (item.findtext("title") or "").strip()
+        link = (item.findtext("link") or "").strip().replace("/abs/", "/pdf/")
+
+        publications.append(
+            {
+                "pubDate": pubDate,
+                "title": title,
+                "link": link,
+            }
+        )
+
+    df = pd.DataFrame.from_records(publications)
+    df = df[df["pubDate"] == date.today().isoformat()]
+
+    return df
+
+
+if __name__ == "__main__":
+    df = download()
+    if df.empty:
+        logger.info("No new publications for today")
+    else:
+        logger.info("Saving publications...")
+
+        df = pd.concat([pd.read_csv(FILE), df])
+        df.drop_duplicates().to_csv(FILE, index=False)
