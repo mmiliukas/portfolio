4bdee4a682167318b6f60ed63fafad72601116b9
Mantas Miliukas <mmiliukas@gmail.com>
2026-06-03 20:07:09 +0300

    chore: update user agent

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ RSS_URL = f"https://rss.arxiv.org/rss/{TOPIC}"
 PUBLICATIONS_FILE = "./publications.csv"
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
 }
