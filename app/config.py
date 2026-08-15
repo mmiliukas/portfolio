e4e74a80650330beee2543fe0190fcfc2bc18fd3
Mantas Miliukas <mmiliukas@gmail.com>
2026-08-15 15:15:20 +0300

    chore: cleanup unused files anymore

--- app/config.py
+++
@@ -1,8 +0,0 @@
-TOPIC = "q-fin.PM"
-RSS_URL = f"https://rss.arxiv.org/rss/{TOPIC}"
-
-PUBLICATIONS_FILE = "./publications.csv"
-
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-}
